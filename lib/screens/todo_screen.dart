bc2827882181657b4edcbd70b7754daa4b93543c
saiusesgithub <saisrujantesting@gmail.com>
2025-06-11 19:26:48 +0530

    added fonts,setup background,appbar,navigationbar

---
+++ lib/screens/todo_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
+
+  @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(18, 18, 18, 1.0),
+      appBar: AppBar(
+        title: Text("DailyDone", style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(13, 13, 13, 1.0),
+        foregroundColor: Color.fromRGBO(255, 94, 87, 1.0),
+        shadowColor: Color.fromRGBO(0, 0, 0, 0.2),
+        leading: Icon(Icons.menu_outlined),
+        actions: [Icon(Icons.search)],
+        actionsPadding: EdgeInsets.only(right: 12),
+      ),
+      body: Container(),
+
+      bottomNavigationBar: NavigationBar(
+        indicatorColor: Color.fromRGBO(255, 94, 87, 1.0),
+        backgroundColor: Color.fromRGBO(13, 13, 13, 1.0),
+        destinations: <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.checklist_outlined),
+            label: 'Todo',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.whatshot_outlined),
+            label: 'Habits',
+          ),
+        ],
+      ),
+    );
+  }
+}
